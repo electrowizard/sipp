6f29b02c5f885e0382d73192d52e6794ec48e6a6
electrowizard/Sathvik Prasad <sathviknp@gmail.com>
2017-01-21 23:04:28 +0530

    Successfuly added a xmlgen.cpp and xmlgen.h files and also modified the Makefile.am to include the files for the build path Added printf statements and successfuly called it using the command line arguement --xmlgen

---
+++ include/xmlgen.h
@@ -0,0 +1,13 @@
+
+#ifndef __SIPP_XMLGEN_H__
+#define __SIPP_XMLGEN_H__
+#define satprasa_test "TestString"
+#include <stdio.h>
+//#include "defines.h"
+
+
+void xmlgen_f1(void);
+
+#endif /* __SIPP_XMLGEN_H__ */
+
+
---
+++ src/xmlgen.cpp
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <xmlgen.h>
+
+void xmlgen_f1 (void) {
+
+printf("Going into the cpp function call.\n\n :-)");
+}
